3e6ce4a68b5b892673b987608c7606a36db2aa91
Jesse Boyer <aniyishay@gmail.com>
2015-07-23 03:53:11 -0400

    Remove NodeJS not needed

--- bin/ppa.sh
+++ bin/ppa.sh
@@ -5,7 +5,6 @@ sudo apt-get install -y python-software-properties
 sudo add-apt-repository -y ppa:git-core/ppa
 sudo add-apt-repository -y ppa:numix/ppa
 sudo add-apt-repository -y ppa:ubuntu-wine/ppa
-sudo add-apt-repository -y ppa:chris-lea/node.js
 # Phalcon was moved to the phalcon sh
 
 echo "(+) Updating Sources List"
