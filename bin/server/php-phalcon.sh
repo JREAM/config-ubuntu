36066ff4a564ffb5f75dfc1de12febd297db460b
Jesse Boyer <imboyus@gmail.com>
2017-02-26 19:39:19 -0500

    phalcon different installation way

--- bin/server/php-phalcon.sh
+++ bin/server/php-phalcon.sh
@@ -4,21 +4,13 @@ if [[ ! $INSTALL_SCRIPT ]]; then
     exit
 fi
 
-if [ -f /etc/apt/sources.list.d/phalcon*ppa ]; then
-    sudo rm /etc/apt/sources.list.d/phalcon*ppa
-fi
-sudo add-apt-repository -y ppa:phalcon/stable
-echo "(+) Re-Adding Phalcon PPA."
-
-# Update to get the new repo details
-sudo apt-get update
+curl -s https://packagecloud.io/install/repositories/phalcon/stable/script.deb.sh | sudo bash
 
 echo "(+) Installing Phalcon."
 
 if [[ $OS_CODENAME == 'xenial' ]]; then
     # PHP7 Phalcon
     sudo apt-get install -y php7.0-phalcon
-    sudo sh -c 'echo "extension=phalcon.so" >> /etc/php/7.0/mods-available/phalcon.ini'
     sudo phpenmod phalcon
 else
     # PHP5 Phalcon
@@ -30,7 +22,6 @@ echo "(+) Installing Phalcon Devtools."
 echo $PROJECT_TEMP_PATH
 echo '{"require":{"phalcon/devtools":"dev-master"}}' > $PROJECT_TEMP_PATH/composer.json
 cd $PROJECT_TEMP_PATH
-sudo composer install  # For Permission Issues :\
 
 # Move the composer install items we want to a good spot
 sudo mv vendor/phalcon/devtools/* $DEST_DIR
