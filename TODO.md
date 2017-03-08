a3c67c19c5989a34299d1f02315f8b4762367cd4
Jesse Boyer <imboyus@gmail.com>
2017-03-07 22:41:18 -0500

    Composer update for perfect paths, .profile has autocomplete and COMPOSER_HOME system variable

---
+++ TODO.md
@@ -0,0 +1,11 @@
+# TODO
+
+- Ubuntu 16.10
+    - new users have no ~/.config
+    - .composer globals install to: `~/.config/composer` (not dot)
+- Ubuntu 16.04
+    - if root (not recoomended) it goes into /root/.config/composer
+    - .composer globals install to `~/.composer/` (dot)
+
+
+
--- bin/server/php-composer.sh
+++ bin/server/php-composer.sh
@@ -4,50 +4,79 @@ if [[ ! $INSTALL_SCRIPT ]]; then
     exit
 fi
 
-echo "(+) Installing/Updating PHP Composer"
+echo ""
+echo "(+) Running as $SUDO_USER, If this is not ROOT, disregard warnings"
+echo ""
+
+# ------------------------------------
+# Get composer home path
+#   *does NOT end in trailing slash
+# Add sticky permissions
+# ------------------------------------
+COMPOSER_HOME=`composer config -g home`
+
+if [ ! -d "$COMPOSER_HOME" ]; then
+    mkdir "$COMPOSER_HOME"
+    chmod g+rws "$COMPOSER_HOME"
+fi
+
 # See if we need composer (Duplicate in Phalcon for now)
 if [ ! -f /usr/local/bin/composer ]; then
-    curl -sS https://getcomposer.org/installer | php && sudo mv composer.phar /usr/local/bin/composer
-
-    # Add permissions to the composer folder
-    sudo chown -R $USER:$USER $HOME_PATH/.composer
-    sudo chmod g+rws $HOME_PATH/.composer
+    # ------------------------------------
+    # Download/Install Compose
+    # Move to /usr/local/bin for all users
+    # ------------------------------------
+    echo "(+) Installing PHP Composer."
+    curl -sS https://getcomposer.org/installer | php
+    echo "(+) Moved composer to /usr/local/bin/composer"
+    sudo mv composer.phar /usr/local/bin/composer
+    echo "(+) Installing Composer Autocomplete"
+    composer global require bamarni/symfony-console-autocomplete
+    echo "(+) Installing Important global Packages"
+    composer global require --prefer-dist hirak/prestissimo
+    composer global require --prefer-dist phpunit/phpunit
+    composer global require --prefer-dist phpunit/dbunit
+    composer global require --prefer-dist phpunit/php-code-coverage
+    composer global require --prefer-dist phpunit/phpunit-mock-objects
+    composer global require --prefer-dist phing/phing
+    composer global require --prefer-dist sebastian/phpcpd
+    composer global require --prefer-dist phploc/phploc
+    composer global require --prefer-dist phpmd/phpmd
+    composer global require --prefer-dist squizlabs/php_codesniffer
+    composer global require --prefer-dist phpdocumentor/phpdocumentor:2.*
+    echo "( + ) Done installing composer packages"
 else
-    echo $USER
-    # Add permissions to the composer & cache folder BEFORE updating
-    sudo chown -R $USER:$USER $HOME_PATH/.composer
-    sudo chmod g+rws $HOME_PATH/.composer
+    # ------------------------------------
+    # Update: Composer
+    # Update: Composer Autocompletions
+    # ------------------------------------
+    echo "(+) Updating PHP Composer."
+    composer self-update
 
-    sudo composer self-update
+    echo "(+) Updating Global Composer Packages (--prefer-dist)"
+    composer update
+
+    if [ -d "$COMPOSER_HOME/vendor/bamarni/symfony-console-autocomplete" ]; then
+        # Exists, Update
+        composer global update bamarni/symfony-console-autocomplete
+    else
+        # Doesn't exist, Install
+        composer global require bamarni/symfony-console-autocomplete
+    fi
+
+    echo "(+) Done updating composer packages"
+
 fi
 
-echo "(+) Installing/Updating composer packages for $USER"
 
-composer global require --prefer-dist hirak/prestissimo
-composer global require --prefer-dist phpunit/phpunit
-composer global require --prefer-dist phpunit/dbunit
-composer global require --prefer-dist phpunit/php-code-coverage
-composer global require --prefer-dist phpunit/phpunit-mock-objects
-composer global require --prefer-dist phing/phing
-composer global require --prefer-dist sebastian/phpcpd
-composer global require --prefer-dist phploc/phploc
-composer global require --prefer-dist phpmd/phpmd
-composer global require --prefer-dist squizlabs/php_codesniffer
-composer global require --prefer-dist phpdocumentor/phpdocumentor:2.*
+echo '# ---------------------------------------------
+ If you did not run the `dot` installation,
+ please look at dotfiles/.profile and under
+ the PHP section.
 
-sudo chown $USER:$USER -R $HOME_PATH/.composer
+ If you update your .profile, run:
 
-echo "( + ) Done installing composer packages"
-echo '( + ) Ensure your $HOME_PATH/.profile has the following:
-# ---------------------------------------------
-
-if [ -d "$USER/.composer/vendor/bin" ] ; then
-    PATH="$USER/.composer/vendor/bin:$PATH"
-fi
-
-# ---------------------------------------------
-
-Then run $ source $HOME_PATH/.profile
+    $ source $HOME_PATH/.profile
 
 # ---------------------------------------------
 '
