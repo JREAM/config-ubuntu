0af8938f404f4c593c63b3a8ae6217dcb297ce74
Jesse Boyer <imboyus@gmail.com>
2017-03-08 22:57:48 -0500

    apache project now only has index.php file with apache.md in modal popup

--- files/projects/index.php
+++ files/projects/index.php
@@ -3,8 +3,14 @@
 <head>
     <meta charset="utf-8">
     <title>Localhost</title>
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link href='//fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha256-/SIrNqv8h6QGKDuNoLGA4iret+kyesCkHGzVUUV0shc=" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <script>
+
+    </script>
     <style>
     body {
         padding-bottom: 20px;
@@ -64,9 +70,13 @@
     <div class="header">
     <div class="container">
             <div class="col-md-4">
-                <h3 class="text-center">Localhost</h3>
+                <h3 class="text-center"><b>local</b>host</h3>
             </div>
             <div class="col-md-8">
+
+                <button type="button" class="btn btn-default btn-sm pull-right" data-toggle="modal" data-target="#apache-modal">
+                    Apache VHost Help
+                </button> |
             </div>
     </div>
     </div>
@@ -117,6 +127,65 @@
             </div>
         </div>
     </div>
+
+<div class="modal fade" id="apache-modal" tabindex="-1" role="dialog">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title">Apache VirtualHost Setup</h4>
+      </div>
+      <div class="modal-body">
+
+            <p>
+            If you'd like to setup a VirtualHost other than this one, follow these simple
+            instructions.
+            </p>
+
+            <ol>
+                <li>First, make the folder where your project(s) will live.</li>
+                <li>I'll use an example: <code>mkdir -p /var/www/<b>unicorn</b></code></li>
+                <li>Next, we have to create a VirtualHost, you should name it something like your project folder.
+                <li><code>sudo vim /etc/apache2/sites-available/<b>unicorn.conf</b></code></li>
+            </ol>
+
+<pre>
+&lt;VirtualHost *:80&gt;
+    ServerName <b>unicorn</b>
+    DocumentRoot "<b>/var/www/unicorn</b>"
+&lt;/VirtualHost&gt;
+
+&lt;Directory <b>/var/www/unicorn</b>&gt;
+    Options Indexes FollowSymLinks MultiViews
+    AllowOverride All
+    Require all granted
+&lt;/Directory&gt;
+</pre>
+
+            <ol>
+                <li>Enable the site with <code>sudo a2ensite unicorn.conf</code></li>
+                <li>Make sure your directory exists before restarting, otherwise errors occur.</li>
+                <li>Activate the site with <code>sudo service apache2 restart</code></li>
+            </ol>
+
+            <h4>Edit Hosts File</h4>
+
+            <ul>
+                <li>Lastly, edit the hosts file: <code>sudo vim /etc/hosts</code> and append <b>unicorn</b></li>
+            </ul>
+
+<pre>
+; It should look like this:
+127.0.0.1 localhost projects <b>unicorn</b>
+</pre>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+
 
 </body>
 </html>
