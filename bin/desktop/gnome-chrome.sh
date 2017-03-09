e36b9a4f9870c134fcae0ce425d100d785a15f78
Jesse Boyer <imboyus@gmail.com>
2017-03-09 18:45:07 -0500

    gnome chrome was missing

---
+++ bin/desktop/gnome-chrome.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [[ ! $INSTALL_SCRIPT ]]; then
+    echo "(!) Error: You must use the installer script."
+    exit
+fi
+
+
+sudo apt-get install chrome-gnome-shell
+
+echo "(+) Complete! Open Chrome and install extensions at:"
+echo "    https://extensions.gnome.org/local/"
+
+if [ $SKIP_SLEEP == false ]; then
+    sleep 4
+fi
