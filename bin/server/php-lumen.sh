5af543c982ff1e13ab8891ae7dafa56654077eb1
Jesse Boyer <JREAM@users.noreply.github.com>
2017-03-10 23:28:49 -0500

    Create php-lumen.sh

---
+++ bin/server/php-lumen.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+if [[ ! $INSTALL_SCRIPT ]]; then
+    echo "(!) Error: You must use the installer script."
+    exit
+fi
+
+# Make sure permissions to the composer folder are correct
+sudo chown -R $USER:$USER $HOME_PATH/.composer
+sudo chmod g+rw $HOME_PATH/.composer
+
+# TODO: Check if dir exists
+sudo chmod gu+x $HOME_PATH/.composer/bin
+
+composer global require "laravel/lumen-installer"
+
+echo "(+) Complete, run with $ lumen"
+
+if [ $SKIP_SLEEP == false ]; then
+    sleep 4
+fi
