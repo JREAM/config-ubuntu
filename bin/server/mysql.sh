f73e0f71ffa2fa5ac7561a1140a3b1e89d9f5eac
Jesse Boyer <imboyus@gmail.com>
2017-03-25 19:19:30 -0400

    slow query logging on mysql

--- bin/server/mysql.sh
+++ bin/server/mysql.sh
@@ -11,6 +11,13 @@ sudo apt-get install -y\
 
 # IMPORTANT: Do NOT have a trailing \ on the LAST item!
 
+# /var/log/mysql/mysql-slow.log
+echo "(+) Enabling Logging for slow queries, and default slow query time (2 sec)"
+
+sudo sed -i '/log_slow_queries/s/#//g' /etc/mysql/mysql.conf.d/mysqld.cnf
+sudo sed -i '/long_query_time/s/#//g' /etc/mysql/mysql.conf.d/mysqld.cnf
+
+
 echo "(+) Complete! Installed MySQL Utilities."
 echo "    MySQL Server, Client, Utilities"
 
