b93286ac5057329d0efc1a1a849aa4f2232d4e7f
Jesse Boyer <imboyus@gmail.com>
2017-03-28 22:30:41 -0400

    add node nvm

--- bin/server/node.sh
+++ bin/server/node.sh
@@ -19,9 +19,17 @@ sudo npm cache clean -f
 sudo npm install -g n
 sudo n stable
 
-echo "(+) Complete! Run with $ node and $ npm."
+echo "(+) Installing 'nvm'"
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
+
+
+echo "(+) Complete! Run with $ node and $ npm. $ command -v nvm"
 echo "(!) If you cannot install a --global/-g package"
 echo "    run the 'permissions' command."
+
+echo '(!) For NVM, set this in your .bashrc or .profile or .zsh:'
+echo 'export NVM_DIR="$HOME/.nvm'
+echo '[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm'
 
 if [ $SKIP_SLEEP == false ]; then
     sleep 4
