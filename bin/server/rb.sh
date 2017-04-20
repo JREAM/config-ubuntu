855dcfb912bf7d2bc43008ebc4db66ff7635be82
Jesse Boyer <imboyus@gmail.com>
2017-04-19 21:19:32 -0400

    updated ruby and dotfile for faster gem install --no docs

--- bin/server/rb.sh
+++ bin/server/rb.sh
@@ -4,6 +4,7 @@ if [[ ! $INSTALL_SCRIPT ]]; then
     exit
 fi
 
+echo "(+) Install Ruby 2.3 and Gem Package Manager"
 sudo apt-get install -y\
     ruby2.3\
     ruby2.3-dev\
@@ -11,8 +12,16 @@ sudo apt-get install -y\
 
 # IMPORTANT: Do NOT have a trailing \ on the LAST item!
 
+echo "(+) Installing RVM and Bundler"
 
-sudo gem install bundler
+# Install gpg2
+command curl -sSL https://rvm.io/mpapis.asc | gpg2 --import -
+
+sudo gem install rvm bundler
+
+echo "(+) Sourcing $HOME_PATH/.rvm/scripts/rvm"
+
+source "$HOME_PATH/.rvm/scripts/rvm"
 
 echo "(+) Complete! Run with $ ruby, and $ gem"
 
