8535cfe86a438e3e9d175f7594d4c6ded5504d5a
Jesse Boyer <imboyus@gmail.com>
2018-07-29 22:00:55 -0400

    docker compose version update

--- bin/devops/docker.sh
+++ bin/devops/docker.sh
@@ -4,6 +4,8 @@ if [[ ! $INSTALL_SCRIPT ]]; then
     exit
 fi
 
+COMPOSE_VERSION="1.22.0"
+
 echo "(+) Installing Docker CE | First Removing Old Docker Engine"
 sudo apt-get remove docker docker-engine
 
@@ -32,18 +34,17 @@ sudo groupadd docker
 sudo usermod -aG docker $USER
 sudo usermod -aG docker root
 
-echo "(+) Installing: Compose"
-sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose \
+echo "(+) Installing: Docker Compose ($COMPOSE_VERSION)"
+sudo curl -L https://github.com/docker/compose/releases/download/$COMPOSE_VERSION/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose \
   && chmod +x /usr/local/bin/docker-compose
 
 echo "(+) Adding: bash-completion for docker-compose"
 sudo url -L https://raw.githubusercontent.com/docker/compose/$(docker-compose version --short)/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
 
 
-echo "(+) Complete! Run with $ docker / docker-compose"
-echo ""
-echo "    If you are using UFW, please see:"
-echo "    https://docs.docker.com/engine/installation/linux/ubuntulinux/#/enable-ufw-forwarding"
+echo "(+) Complete! Run with $ docker / $ docker-compose"
+echo -e "\t You may need to reload your shell to apply the new user group permissions."
+echo -e "\t Which were: sudo usermod -aG docker \$USER"
 
 if [ $SKIP_SLEEP == false ]; then
     sleep 4
