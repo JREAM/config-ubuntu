5eec0b4e4d3a51805ed6c5b954be8ede3dd45ee5
Jesse Boyer <imboyus@gmail.com>
2018-10-24 04:18:11 -0400

    nemo

--- bin/desktop/nemo.sh
+++ bin/desktop/nemo.sh
@@ -13,7 +13,6 @@ sudo add-apt-repository ppa:embrosyn/cinnamon
 
 sudo apt-get install -y\
   nemo \
-  nemo-dropbox \
   nemo-preview \
   nemo-emblems \
   nemo-media-columns \
@@ -25,6 +24,13 @@ sudo apt -y remove nautilus-dropbox
 # Dropbox
 wget http://packages.linuxmint.com/pool/import/d/dropbox/dropbox_2015.10.28_amd64.deb
 sudo dpkg -i dropbox*.deb && rm dropbox*.deb
+
+sudo apt install -y nemo-dropbox
+
+echo "(+) Complete! To set nemo as your default file program run:"
+echo "    $ xdg-mime default nemo.desktop inode/directory application/x-gnome-saved-search"
+echo "(+) If you want to set Nautilus back to the default program run:"
+echo "    $ xdg-mime default nautilus.desktop inode/directory application/x-gnome-saved-search"
 
 
 if [ $SKIP_SLEEP == false ]; then
