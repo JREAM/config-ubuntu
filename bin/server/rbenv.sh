2b86b51bf360fbe244dfe6d891e0f2ec6ef62e33
Jesse Boyer <imboyus@gmail.com>
2019-01-27 19:36:48 -0500

    fix rbenv, wrong name lol

--- bin/server/rbenv.sh
+++ bin/server/rbenv.sh
@@ -4,7 +4,7 @@ if [[ ! $INSTALL_SCRIPT ]]; then
     exit
 fi
 
-echo "(+) Install (RVM) Ruby Version Manager"
+echo "(+) Install (RBENV) Ruby Version Manager"
 
 # Dependencies
 sudo apt-get install -y\
@@ -17,23 +17,23 @@ sudo apt-get install -y\
   zlib1g-dev\
   libncurses5-dev\
   libffi-dev\
-  libgdbm3\
+  libgdbm5\
   libgdbm-dev
 
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv &&\
-  git clone https://github.com/rbenv/ruby-build.git $HOME_PATH/.rbenv/plugins/ruby-build
+git clone https://github.com/rbenv/ruby-build.git $HOME_PATH/.rbenv/plugins/ruby-build
 
-sudo chown -r $USER:$USER $HOME_PATH/.rbenv
+sudo chown -R $USER:$USER $HOME_PATH/.rbenv
 # IMPORTANT: Do NOT have a trailing \ on the LAST item!
 
 echo "(+) Complete! Make sure to add the following:"
 echo ""
 echo "    export PATH=\"$HOME/.rbenv/bin:$PATH\"' >> ~/.bashrc"
-echo "    eval \"$(rbenv init -)\"' >> ~/.bashrc"
+echo "    eval \"\$(rbenv init -)\"\' >> ~/.bashrc"
 echo "(+) Once complete, source bashrc and here's an example:"
 echo "    $ source ~/.bashrc"
-echo "    $ rbenv install 2.5.1"
-echo "    $ rbenv global 2.5.1"
+echo "    $ rbenv install 2.5.3"
+echo "    $ rbenv global 2.5.3"
 
 if [ $SKIP_SLEEP == false ]; then
     sleep 4
