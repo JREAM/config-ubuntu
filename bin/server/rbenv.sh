d2a0069cbdd1acd591d31f5c667409382b679a52
Jesse Boyer <imboyus@gmail.com>
2019-01-27 19:41:46 -0500

    oy

--- bin/server/rbenv.sh
+++ bin/server/rbenv.sh
@@ -7,34 +7,20 @@ fi
 echo "(+) Install (RBENV) Ruby Version Manager"
 
 # Dependencies
-sudo apt-get install -y\
-  autoconf\
-  bison\
-  build-essential\
-  libssl-dev\
-  libyaml-dev\
-  libreadline6-dev\
-  zlib1g-dev\
-  libncurses5-dev\
-  libffi-dev\
-  libgdbm5\
-  libgdbm-dev
+sudo apt-get install -y rbenv ruby-build
 
-git clone https://github.com/rbenv/rbenv.git ~/.rbenv &&\
-git clone https://github.com/rbenv/ruby-build.git $HOME_PATH/.rbenv/plugins/ruby-build
+# Remove old folder no longe rused
+if [ -d $HOME_PATH/.rbenv ]; then
+  rm -rf $HOME_PATH/.rbenv
+fi
 
-sudo chown -R $USER:$USER $HOME_PATH/.rbenv
 # IMPORTANT: Do NOT have a trailing \ on the LAST item!
 
-echo "(+) Complete! Make sure to add the following:"
-echo ""
-echo "    export PATH=\"$HOME/.rbenv/bin:$PATH\"' >> ~/.bashrc"
-echo "    eval \"\$(rbenv init -)\"\' >> ~/.bashrc"
-echo "(+) Once complete, source bashrc and here's an example:"
-echo "    $ source ~/.bashrc"
-echo "    $ rbenv install 2.5.3"
+echo "(+) Complete!"
+echo "    $ rbenv install -l"
 echo "    $ rbenv global 2.5.3"
-
+echo ""
+echo ""
 if [ $SKIP_SLEEP == false ]; then
     sleep 4
 fi
