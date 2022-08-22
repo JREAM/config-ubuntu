3b68b9040e66f2a2f010106ea16b79dad18ac680
Jesse Boyer <imboyus@gmail.com>
2022-08-21 22:24:06 -0400

    forgot update

--- bin/devops/docker.sh
+++ bin/devops/docker.sh
@@ -24,6 +24,8 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 
+apt-get update
+
 echo "(+) Installing Docker"
 sudo apt-get install -y \
   docker-ce \
