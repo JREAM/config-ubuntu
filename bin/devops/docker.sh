28c2d9f60f1693ccd515cc7387fc57a2fea09251
Jesse Boyer <imboyus@gmail.com>
2022-08-21 22:21:42 -0400

    docker upd

--- bin/devops/docker.sh
+++ bin/devops/docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [[ ! $INSTALL_SCRIPT ]]; then
-    echo "(!) Error: You must use the installer script."
-    exit
+  echo "(!) Error: You must use the installer script."
+  exit
 fi
 
 COMPOSE_VERSION="1.23.2"
@@ -10,10 +10,10 @@ echo "(+) Installing Docker CE | First Removing Old Docker Engine"
 sudo apt-get remove -y docker docker-engine docker.io containerd runc
 
 sudo apt-get -y install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
+  ca-certificates \
+  curl \
+  gnupg \
+  lsb-release
 
 echo "(+) Installing Docker GPG Key"
 sudo mkdir -p /etc/apt/keyrings
@@ -22,7 +22,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo "(+) Updating the /etc/sources.list.d/"
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 
 echo "(+) Installing Docker"
 sudo apt-get install -y \
@@ -37,16 +37,10 @@ sudo groupadd docker
 sudo usermod -aG docker $USER
 sudo usermod -aG docker root
 
-# Docker Compose
-echo "(+) Installing: Docker Compose ($COMPOSE_VERSION)"
-sudo curl -L "https://github.com/docker/compose/releases/download/$COMPOSE_VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && sudo chmod +x /usr/local/bin/docker-compose"
-
-
-
 echo "(+) Complete! Run with $ docker / $ docker-compose"
 echo -e "\t You may need to reload your shell to apply the new user group permissions."
 echo -e "\t Which were: sudo usermod -aG docker \$USER"
 
 if [ $SKIP_SLEEP == false ]; then
-    sleep 4
+  sleep 4
 fi
